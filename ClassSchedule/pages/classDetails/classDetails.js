a847469d26df288ff749036f6139efc6e8a7dadf
cqs <cqs@rrtimes.com>
2018-07-11 23:33:04 +0800

    课程列表

--- ClassSchedule/pages/classDetails/classDetails.js
+++ ClassSchedule/pages/classDetails/classDetails.js
@@ -17,7 +17,10 @@ Page({
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
-    if (true) {//!!this.data.list && this.data.list[0]
+    this.getTimeData();//时间轴数据
+    this.getTabData();//课程数据
+
+    if (!false) {//!!this.data.list && this.data.list[0]
       this.setData({
         noClassDis: 'none',
         haveClassDis: 'block'
@@ -29,4 +32,128 @@ Page({
       });
     }
   },
+
+  /* 课程详情 */
+  addNewAgenda: function (event) {
+    console.log(event)
+    wx.navigateTo({
+        url: '../setAgenda/setAgenda',
+    })
+  },
+
+
+  //时间线数据
+  getTimeData: function () {
+    var list = [
+      {
+        time: "08:00"
+      },
+      {
+        time: "09:00"
+      },
+      {
+        time: "10:00"
+      },
+      {
+        time: "11:00"
+      },
+      {
+        time: "12:00"
+      },
+      {
+        time: "13:00"
+      },
+      {
+        time: "14:00"
+      },
+      {
+        time: "15:00"
+      },
+      {
+        time: "16:00"
+      },
+      {
+        time: "17:00"
+      },
+      {
+        time: "18:00"
+      },
+      {
+        time: "19:00"
+      },
+      {
+        time: "20:00"
+      },
+      {
+        time: "21:00"
+      },
+      {
+        time: "22:00"
+      },
+      {
+        time: "23:00"
+      },
+      {
+        time: "24:00"
+      }
+    ];
+    this.setData({
+      timeList: list
+    });
+  },
+
+  //课程表数据
+  getTabData: function () {
+    var list = [
+      { id: 0, startTime: '07:00', endTime: '08:00', schoolName: '学而思', className: '数学课', isRemind: 0},
+      { id: 1, startTime: '08:30', endTime: '9:25', schoolName: '新东方', className: '英语课', isRemind: 1},
+      { id: 2, startTime: '22:45', endTime: '23:59', schoolName: '艺欣艺术学校', className: '钢琴课', isRemind: 2}
+    ];
+
+    list = setDataLocation(list);
+
+    this.setData({
+      tableList: list
+    });
+  }
+
 })
+
+function setDataLocation(list) {
+  for(let i=0;i<list.length;i++){
+    let item = list[i],
+      startHour = Number(item.startTime.split(':')[0]),
+      startMinute = Number(item.startTime.split(':')[1]),
+      endHour = Number(item.endTime.split(':')[0]),
+      endMinute = Number(item.endTime.split(':')[1]),
+      height = 3,
+      marginTop = 0
+
+    console.log('start:', startHour, startMinute, 'end:', endHour, endMinute)
+
+    if (endHour - startHour == 1){
+      height = height + 56
+
+      if (!!endMinute){//后期加精度去掉
+        height = height + 59
+      }
+    } else if (endHour - startHour > 1){
+      height = height + 56 + 59 * (endHour - startHour - 1)
+    }
+    // height = height + (endMinute - startMinute)
+
+    if(startHour - 7 > 0){
+      if (startHour - 7 == 1){
+        marginTop = 60
+      } else if (startHour - 7 > 1){
+        marginTop = 60 * (startHour - 7)
+      }
+    }
+    // marginTop = marginTop + startMinute
+
+    item.height = height*2
+    item.marginTop = marginTop*2
+  }
+
+  return list
+}
--- ClassSchedule/pages/setAgenda/setAgenda.js
+++ ClassSchedule/pages/setAgenda/setAgenda.js
@@ -9,18 +9,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-    className: '',
-    schoolName: '',
-
-    startTime: '07:00',
-    endTime: '08:00',
-
-    address: '',
-    teacher: '',
-    phone: '',
-
     date: currentDate,
 
+    className: '',//课程名称
+    schoolName: '',//学校名称
+
+    startTime: '07:00',//开始时间
+    endTime: '08:00',//结束时间
+
+    address: '',//地址
+    teacher: '',//老师姓名
+    phone: '',//联系方式
+
+    //提醒时间
     remind: ['无', '准时', '提前十分钟', '提前半小时', '提前两小时', '提前一天'],
     remindList: [
       { id: 0, name: '无' },
@@ -30,7 +31,9 @@ Page({
       { id: 4, name: '提前两小时' },
       { id: 5, name: '提前一天' }
     ],
-    remindValue: 0
+    remindValue: 0,
+
+    remark:''//备注
   },
   onLoad: function () {
     
@@ -83,16 +86,16 @@ Page({
       phone: e.detail.value
     })
   },
-  //选择生日
-  bindDateChange: function (e) {
-    this.setData({
-      date: e.detail.value
-    })
-  },
-  //选择性别
+  //选择提醒方式
   bindPickerChange: function (e) {
     this.setData({
       remindValue: e.detail.value
+    })
+  },
+  //录入备注
+  setRemarkInput: function (e) {
+    this.setData({
+      remark: e.detail.value
     })
   },
   
